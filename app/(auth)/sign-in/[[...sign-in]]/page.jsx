29c24f5e06b84c1602f71299759aebc34e2d1b0e
Paramjeet Kaur <paramsohal05@gmail.com>
2025-08-29 12:43:44 +0530

    Update page.jsx

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -1,9 +1,5 @@
 import { SignIn } from "@clerk/nextjs";
-import { useEffect } from "react";
 
 export default function Page() {
-   useEffect(()=>{
-     document.title='Splitr-Sign In'
-   })
   return <SignIn />;
 }
