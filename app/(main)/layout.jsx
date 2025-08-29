49a3c91d70dd960a627d1ae1ddf4c0e233a5bab2
Paramjeet Kaur <paramsohal05@gmail.com>
2025-08-29 12:59:57 +0530

    Update layout.jsx

--- app/(main)/layout.jsx
+++ app/(main)/layout.jsx
@@ -1,11 +1,9 @@
 "use client";
 
 import { Authenticated } from "convex/react";
-import React, { useEffect, useState } from "react";
-
+import React from "react";
 
 const MainLayout = ({ children}) => {
-
   return (
     <Authenticated>
       <div className="container mx-auto mt-24 mb-20 px-4 bg-background min-h-screen">
