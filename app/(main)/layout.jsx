f858928e2b006b3d616fcbc94ab53520a13c4d35
Karthik Balasubramanian <karthikbalasubramanian08@gmail.com>
2025-09-17 23:40:07 +0530

    add user-initializer to layout

--- app/(main)/layout.jsx
+++ app/(main)/layout.jsx
@@ -1,14 +1,17 @@
 "use client";
 
 import { Authenticated } from "convex/react";
+import { UserInitializer } from "@/components/user-initializer";
 import React from "react";
 
 const MainLayout = ({ children}) => {
   return (
     <Authenticated>
-      <div className="container mx-auto mt-24 mb-20 px-4 bg-background min-h-screen">
-        {children}
-      </div>
+      <UserInitializer>
+        <div className="container mx-auto mt-24 mb-20 px-4 bg-background min-h-screen">
+          {children}
+        </div>
+      </UserInitializer>
     </Authenticated>
   );
 };
